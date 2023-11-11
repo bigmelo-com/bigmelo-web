84128a8bb78b54af0f1c113af0ec002bd8a5795f
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-11-11 11:37:51 -0500

    Update Whatsapp link

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    './pages/**/*.{html,js}',
+    './routes/**/*.{html,js}',
     './components/**/*.{html,js}',
   ],
   theme: {
